88c0d7842bede133f2b89bddb0af59b442867ad4
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-23 17:03:51 +0530

    Create PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.println("Hello");
+		}
+	}
+
+}
