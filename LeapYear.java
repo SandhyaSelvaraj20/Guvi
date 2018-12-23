7a34dbe46af48f5720fd6a6dd4f8159e24c006ca
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-23 16:57:17 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc = new Scanner(System.in);
+      int n=sc.nextInt();
+      if((n%400==0) || (n%4==0 && n%100!=0))
+    	System.out.println("Leap Year");
+      else
+    	  System.out.println("Not a leap year");
+
+
+	}
+
+}
