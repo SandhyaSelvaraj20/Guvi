fc58caeada1f080d976f11227d7ad387243cef03
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-23 17:08:22 +0530

    Create SumNatural.java

---
+++ SumNatural.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumNatural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++) {
+			sum= sum+i;
+		}
+      System.out.println(sum);
+	}
+
+}
