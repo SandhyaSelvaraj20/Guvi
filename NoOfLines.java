27e6977e1aedb82c362bdde5509169137857dafa
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-25 12:05:29 +0530

    Create NoOfLines.java

---
+++ NoOfLines.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NoOfLines {
+
+	public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in);
+		   String str = sc.nextLine();
+		   String arr[] = str.split("\\.");
+		   int c=0;
+		  for(String a:arr) {
+			  c++;
+		  }
+		  System.out.println(c);	    
+	}
+
+}
