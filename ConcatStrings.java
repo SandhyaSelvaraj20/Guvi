72eb9e8a99df694e55fbd86175f00eebce432f1b
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-25 19:52:44 +0530

    Create ConcatStrings.java

---
+++ ConcatStrings.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConcatStrings {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		String str2 = sc.next();
+		System.out.println(str1+str2);
+		sc.close();
+
+	}
+
+}
