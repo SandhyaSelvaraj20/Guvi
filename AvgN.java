bf8d631911f9bed64480c4d6ba978bf7c190ded1
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-26 18:39:26 +0530

    Create AvgN.java

---
+++ AvgN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AvgN {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		   	sum = sum+arr[i];
+		}
+          System.out.println(sum/n);
+          sc.close();
+	}
+
+}
