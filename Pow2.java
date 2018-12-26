c5ecc3c42b8b7b6b3658f3a9f95410667e9af23e
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-26 18:57:03 +0530

    Create Pow2.java

---
+++ Pow2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pow2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		if(x%2==0)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+
+	}
+
+}
