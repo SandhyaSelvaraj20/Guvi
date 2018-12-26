9b76461abfafd98d43cf02d5da9288afb959d3bc
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2018-12-26 18:16:09 +0530

    Create PrintNextNo.java

---
+++ PrintNextNo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrintNextNo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(n+1);
+		sc.close();
+
+	}
+
+}
