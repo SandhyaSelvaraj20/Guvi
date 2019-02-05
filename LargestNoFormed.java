6ffd98364ddeea5b796d483b2424ca60ab014f0f
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2019-02-06 00:42:36 +0530

    Create LargestNoFormed.java

---
+++ LargestNoFormed.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LargestNoFormed {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n =sc.nextInt();
+	     int arr[] = new int[n];
+	     for(int i=0;i<n;i++) {
+	    	 arr[i] = sc.nextInt();
+	     }
+	     for(int i=0;i<n;i++) {
+	    	 for(int j=i+1;j<n;j++) {
+	    		 if(arr[i]<arr[j]) {
+	    			 int temp = arr[i];
+	    			 arr[i] = arr[j];
+	    			 arr[j] = temp;
+	    		 }
+	    	 }
+	     }
+	     for(int a:arr) {
+	    	 System.out.print(a);
+	     }
+	     sc.close();
+	}
+
+}
