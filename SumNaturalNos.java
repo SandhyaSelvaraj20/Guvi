a297c59970d96872156cb930dcd58c239498f2a6
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2019-02-06 22:55:21 +0530

    Create SumNaturalNos.java

---
+++ SumNaturalNos.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumNaturalNos {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
