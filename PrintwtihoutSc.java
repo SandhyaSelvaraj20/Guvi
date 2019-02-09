313405d6518f069d5c3ea1dfd3b1c299a294c397
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2019-02-09 22:49:24 +0530

    Create PrintwtihoutSc.java

---
+++ PrintwtihoutSc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrintwtihoutSc {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		String str=sc.nextLine();
+		if(System.out.printf(str)==null) {
+			
+		}
+	}
+
+}
