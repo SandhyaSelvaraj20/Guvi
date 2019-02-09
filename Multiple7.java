12c7ea1782a99260a7947e87987d6f6320953dd6
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2019-02-09 21:07:57 +0530

    Create Multiple7.java

---
+++ Multiple7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiple7 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	    int n =sc.nextInt();
+	    if(n%7==0)
+	    	System.out.println("yes");
+	    else
+	    	System.out.println("no");
+	    sc.close();
+	}
+
+}
