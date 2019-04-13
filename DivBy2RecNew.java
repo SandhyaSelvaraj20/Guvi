63be4a8b7c3ae486fa47d1a6b94529b1bf398226
SandhyaSelvaraj20 <42066250+SandhyaSelvaraj20@users.noreply.github.com>
2019-04-13 13:40:02 +0530

    Create DivBy2RecNew.java

---
+++ DivBy2RecNew.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DivBy2RecNew {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		while(n%2==0) {
+			if(n%2==0)
+				n = n/2;
+			else 
+				break;
+		}
+		System.out.println(n);
+		sc.close();
+	}
+}
